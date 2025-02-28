e51f5d7c75cacc1dad2ebe09f4a59bd4a6839597
Aiser <wtchel088@gmail.com>
2025-02-28 12:50:40 +0800

    matugen kitty-color update

--- .config/matugen/scripts/fzf.fish
+++ .config/matugen/scripts/fzf.fish
@@ -1,7 +1,7 @@
 #!/bin/fish
 set -Ux FZF_DEFAULT_OPTS "\
-  --color=bg+:#3f373b,bg:#181115,spinner:#f4b9a0,hl:#f8b1dd \
-  --color=fg:#eddfe4,header:#f8b1dd,info:#f4b9a0,pointer:#eddfe4 \
-  --color=marker:#ffb4ab,fg+:#eddfe4,prompt:#f8b1dd,hl+:#f8b1dd \
-  --color=selected-bg:#3f373b \
+  --color=bg+:#3a383e,bg:#141318,spinner:#eeb8cb,hl:#cbbeff \
+  --color=fg:#e6e1e9,header:#cbbeff,info:#eeb8cb,pointer:#e6e1e9 \
+  --color=marker:#ffb4ab,fg+:#e6e1e9,prompt:#cbbeff,hl+:#cbbeff \
+  --color=selected-bg:#3a383e \
   --multi"
