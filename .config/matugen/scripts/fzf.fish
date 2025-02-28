ddde2d4c2cba3a4b3f5fddfd694965dfcc676abc
Aiser <wtchel088@gmail.com>
2025-02-28 11:37:06 +0800

    matugen update

---
+++ .config/matugen/scripts/fzf.fish
@@ -0,0 +1,7 @@
+#!/bin/fish
+set -Ux FZF_DEFAULT_OPTS "\
+  --color=bg+:#3f373b,bg:#181115,spinner:#f4b9a0,hl:#f8b1dd \
+  --color=fg:#eddfe4,header:#f8b1dd,info:#f4b9a0,pointer:#eddfe4 \
+  --color=marker:#ffb4ab,fg+:#eddfe4,prompt:#f8b1dd,hl+:#f8b1dd \
+  --color=selected-bg:#3f373b \
+  --multi"
