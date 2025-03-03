a321a164d7853c46ed4656a00d4ece37bf9029bd
Aiser <wtchel088@gmail.com>
2025-03-03 14:53:55 +0800

    matugen update

--- .config/matugen/scripts/fzf.fish
+++ .config/matugen/scripts/fzf.fish
@@ -1,7 +1,7 @@
 #!/bin/fish
 set -Ux FZF_DEFAULT_OPTS "\
-  --color=bg+:#3a383e,bg:#141318,spinner:#eeb8cb,hl:#cbbeff \
-  --color=fg:#e6e1e9,header:#cbbeff,info:#eeb8cb,pointer:#e6e1e9 \
-  --color=marker:#ffb4ab,fg+:#e6e1e9,prompt:#cbbeff,hl+:#cbbeff \
-  --color=selected-bg:#3a383e \
+  --color=bg+:#343a39,bg:#0e1514,spinner:#aec9e6,hl:#81d5ca \
+  --color=fg:#dde4e2,header:#81d5ca,info:#aec9e6,pointer:#dde4e2 \
+  --color=marker:#ffb4ab,fg+:#dde4e2,prompt:#81d5ca,hl+:#81d5ca \
+  --color=selected-bg:#343a39 \
   --multi"
