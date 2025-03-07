f824a8dca1fb5924e92eee150fbd07b10a39ef6c
Aiser <wtchel088@gmail.com>
2025-03-07 10:56:48 +0800

    discord update

--- .config/matugen/scripts/fzf.fish
+++ .config/matugen/scripts/fzf.fish
@@ -1,7 +1,7 @@
 #!/bin/fish
 set -Ux FZF_DEFAULT_OPTS "\
-  --color=bg+:#343a39,bg:#0e1514,spinner:#aec9e6,hl:#81d5ca \
-  --color=fg:#dde4e2,header:#81d5ca,info:#aec9e6,pointer:#dde4e2 \
-  --color=marker:#ffb4ab,fg+:#dde4e2,prompt:#81d5ca,hl+:#81d5ca \
-  --color=selected-bg:#343a39 \
+  --color=bg+:#363a34,bg:#10140f,spinner:#a1ced5,hl:#9fd49c \
+  --color=fg:#e0e4db,header:#9fd49c,info:#a1ced5,pointer:#e0e4db \
+  --color=marker:#ffb4ab,fg+:#e0e4db,prompt:#9fd49c,hl+:#9fd49c \
+  --color=selected-bg:#363a34 \
   --multi"
