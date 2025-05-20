47dec7656ce259b2ddd5303e5ce994d7aca36ac3
AISER <47445389+acdcbyl@users.noreply.github.com>
2025-05-20 17:52:53 +0800

    Update SystemIndicators.tsx
    删除icon的派生变量，它在早期就会bind network.wired的icon导致某些设备出现type error，现在采用强制地判断尝试修复它

--- ags-gtk4/widget/Bar/items/SystemIndicators.tsx
+++ ags-gtk4/widget/Bar/items/SystemIndicators.tsx
@@ -8,28 +8,34 @@ import AstalBluetooth from "gi://AstalBluetooth";
 
 function NetworkIcon() {
   const network = AstalNetwork.get_default();
-  if (!network.wifi)
+  //三次空值检测
+  if (!network)
+    return null;
+  if (!network.wifi && network.wired)
     return <image iconName={bind(network.wired, "iconName")} />;
-
-  const icon = Variable.derive(
-    [
-      bind(network, "primary"),
-      bind(network.wifi, "iconName"),
-      bind(network.wired, "iconName"),
-    ],
-    (primary, wifiIcon, wiredIcon) => {
-      if (
-        primary == AstalNetwork.Primary.WIRED ||
-        primary == AstalNetwork.Primary.UNKNOWN
-      ) {
-        return wiredIcon;
-      } else {
-        return wifiIcon;
-      }
-    },
-  );
-  return <image iconName={icon()} onDestroy={() => icon.drop()} />;
+  if (!network.wifi && !network.wired)
+    return <image iconName="network-offline" />;
+  //有些设备无法获取到wired的icon，尚不清楚原因
+  // const icon = Variable.derive(
+  //   [
+  //     bind(network, "primary"),
+  //     bind(network.wifi, "iconName"),
+  //     bind(network.wired, "iconName"),
+  //   ],
+  //   (primary, wifiIcon, wiredIcon) => {
+  //     if (
+  //       primary == AstalNetwork.Primary.WIRED ||
+  //       primary == AstalNetwork.Primary.UNKNOWN
+  //     ) {
+  //       return wiredIcon;
+  //     } else {
+  //       return wifiIcon;
+  //     }
+  //   },
+  // );
+  return <image iconName={bind(network.wifi, "iconName")} />;
 }
+
 
 export default function SystemButton() {
   // const battery = AstalBattery.get_default();
