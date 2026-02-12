244a43eff85f16966b5769ae621f9295440639ec
Aiser <2912778691@qq.com>
2026-02-12 10:46:36 +0800

    更新matugen

--- .config/matugen/scripts/fzf.fish
+++ .config/matugen/scripts/fzf.fish
@@ -1,7 +1,7 @@
 #!/bin/fish
 set -Ux FZF_DEFAULT_OPTS "\
-  --color=bg+:#413738,bg:#191112,spinner:#ebbe90,hl:#ffb2be \
-  --color=fg:#f0dee0,header:#ffb2be,info:#ebbe90,pointer:#f0dee0 \
-  --color=marker:#ffb4ab,fg+:#f0dee0,prompt:#ffb2be,hl+:#ffb2be \
-  --color=selected-bg:#413738 \
+  --color=bg+:#3f3736,bg:#181211,spinner:#e0c38c,hl:#ffb4ab \
+  --color=fg:#ede0de,header:#ffb4ab,info:#e0c38c,pointer:#ede0de \
+  --color=marker:#ffb4ab,fg+:#ede0de,prompt:#ffb4ab,hl+:#ffb4ab \
+  --color=selected-bg:#3f3736 \
   --multi"
